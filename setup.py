732cabef2c18b28462efa15c99d7cfe6df77c1fe
Stanislaw Horna <stanislawhorna@outlook.com>
2024-11-21 19:22:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import cfg
 setup(
-    name="THDc_gRPC",
+    name="THDC_PROTO",
     version=cfg.version,
     packages=find_packages(),
     author="THDC",
